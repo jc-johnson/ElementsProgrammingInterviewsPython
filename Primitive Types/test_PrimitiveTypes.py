cfcd34778ff60bc4cd5d8d04de26bbd10a1ec35c
Jordan C. Johnson <jc-johnson@users.noreply.github.com>
2021-05-24 04:32:24 -0500

    Adding test

--- Primitive Types/test_PrimitiveTypes.py
+++ Primitive Types/test_PrimitiveTypes.py
@@ -4,7 +4,8 @@ import PrimitiveTypes
 class TestPrimitiveTypes(unittest.TestCase):
 
     def test_Count_Bits(self):
-        self.assertEqual(PrimitiveTypes.count_bits(11), 15)
+        self.assertEqual(PrimitiveTypes.count_bits(1101), 15)
+        self.assertEqual(PrimitiveTypes.count_bits(1111), 6)
 
 if __name__ == '__main__':
     unittest.main()
